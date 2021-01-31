9b842ed3c75a336b7469d72628b6ca7cecf55fc8
galaxy119 <amathor929@gmail.com>
2021-01-30 21:21:53 -0800

    Yes, my brain IS as smooth as glass, ty for asking!

--- CustomItems/Items/LethalInjection.cs
+++ CustomItems/Items/LethalInjection.cs
@@ -13,7 +13,7 @@ namespace CustomItems.Items
         public override string ItemName { get; set; } = "LJ-119";
 
         public override string ItemDescription { get; set; } =
-            "This is a Lethal Injection that, when used, will cause SCP-196 to immediately leave his enrage, regardless of how many targets he currently has, if you are one of his current targets. You always die when using this, even if there's no enrage to break, or you are not a target.";
+            "This is a Lethal Injection that, when used, will cause SCP-096 to immediately leave his enrage, regardless of how many targets he currently has, if you are one of his current targets. You always die when using this, even if there's no enrage to break, or you are not a target.";
 
         protected override void LoadEvents()
         {
