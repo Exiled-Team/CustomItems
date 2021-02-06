45322ba59d1f98528ee31accb2893133a4892bde
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2021-02-06 20:14:34 +0100

    Scp1499Config

---
+++ CustomItems/Configs/ItemConfigs/Scp1499Config.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using CustomItems.API;
+
+namespace CustomItems.ItemConfigs
+{
+    public class Scp1499Config
+    {
+        [Description("Where on the map items should spawn, and their % chance of spawning in each location.")]
+        public Dictionary<SpawnLocation, float> SpawnLocations { get; set; } = new Dictionary<SpawnLocation, float>
+        {
+            { SpawnLocation.Inside173Armory, 100 }
+        };
+
+        [Description("The Custom Item ID for this item.")]
+        public int Id { get; set; } = 11;
+
+        [Description("The description of this item show to players when they obtain it.")]
+        public string Description { get; set; } = "The gas mask that teleports you to another dimmension, when you put it on.";
+
+        [Description("The name of this item shown to players when they obtain it.")]
+        public string Name { get; set; } = "SCP-1499";
+
+        [Description("How many of this item are allowed to naturally spawn on the map when a round starts. 0 = unlimited")]
+        public int SpawnLimit { get; set; } = 1;
+    }
+}
