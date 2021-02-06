33d8413524d0e82aefd4e446a82f4e7693c092aa
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2021-02-06 20:15:07 +0100

    Scp1499

---
+++ CustomItems/Items/Scp1499.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using CustomItems.API;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using UnityEngine;
+
+namespace CustomItems.Items
+{
+    class Scp1499 : CustomItem
+    {
+        public Scp1499(ItemType type, int itemId) : base(type, itemId)
+        {
+        }
+
+        public override string Name { get; set; } = Plugin.Singleton.Config.ItemConfigs.Scp1499Cfg.Name;
+        public override Dictionary<SpawnLocation, float> SpawnLocations { get; set; } = Plugin.Singleton.Config.ItemConfigs.Scp1499Cfg.SpawnLocations;
+        public override string Description { get; set; } = Plugin.Singleton.Config.ItemConfigs.Scp1499Cfg.Description;
+        public override int SpawnLimit { get; set; } = Plugin.Singleton.Config.ItemConfigs.Scp1499Cfg.SpawnLimit;
+
+
+        private Dictionary<Player, Vector3> scp1499Players = new Dictionary<Player, Vector3>();
+
+
+        protected override void LoadEvents()
+        {
+            Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingForPlayers1499;
+            Exiled.Events.Handlers.Player.MedicalItemUsed += OnUsedMedicalItem;
+            Exiled.Events.Handlers.Player.DroppingItem += OnItemDropping;
+            base.LoadEvents();
+        }
+
+        protected override void UnloadEvents()
+        {
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= OnWaitingForPlayers1499;
+            Exiled.Events.Handlers.Player.MedicalItemUsed -= OnUsedMedicalItem;
+            Exiled.Events.Handlers.Player.DroppingItem -= OnItemDropping;
+            base.UnloadEvents();
+        }
+
+
+        private void OnWaitingForPlayers1499()
+        {
+            scp1499Players.Clear();
+        }
+
+        private void OnUsedMedicalItem(UsedMedicalItemEventArgs ev)
+        {
+            if(CheckItem(ev.Player.CurrentItem))
+            {
+                scp1499Players.Add(ev.Player, ev.Player.Position);
+
+                ev.Player.Position = new UnityEngine.Vector3(152.93f, 978.03f, 93.64f);
+                ev.Player.ReferenceHub.playerEffectsController.DisableEffect<CustomPlayerEffects.Scp268>();
+
+                Timing.CallDelayed(15f, () =>
+                {
+                    if (scp1499Players.ContainsKey(ev.Player))
+                    {
+                        ev.Player.Position = scp1499Players[ev.Player];
+                        scp1499Players.Remove(ev.Player);
+                    }
+                });
+            }
+        }
+
+        private void OnItemDropping(DroppingItemEventArgs ev)
+        {
+            if (scp1499Players.ContainsKey(ev.Player))
+            {
+                ev.IsAllowed = false;
+
+                if(CheckItem(ev.Item))
+                {
+                    ev.Player.RemoveItem(ev.Item);
+
+                    ev.Player.Position = scp1499Players[ev.Player];
+                    GiveItem(ev.Player);
+
+                    scp1499Players.Remove(ev.Player);
+                }
+            }
+        }
+    }
+}
