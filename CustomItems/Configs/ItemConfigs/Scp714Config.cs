388d2d55cf71095dbed0c77189b30a1b5519d617
Michal78900 <michal.dlugopis@gmail.com>
2021-02-25 15:53:17 +0100

    Added 2 new .cs files

---
+++ CustomItems/Configs/ItemConfigs/Scp714Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomItems.Configs.ItemConfigs
+{
+    class Scp714Config
+    {
+    }
+}
