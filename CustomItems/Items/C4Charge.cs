ebd8c08eb8f428e44840b63d97020bc6010f504a
Michal78900 <michal.dlugopis@gmail.com>
2021-03-05 13:50:03 +0100

    Added C4 Charge

---
+++ CustomItems/Items/C4Charge.cs
@@ -0,0 +1,224 @@
+﻿namespace CustomItems.Items
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using Exiled.CustomItems.API;
+    using Exiled.CustomItems.API.Features;
+    using Exiled.CustomItems.API.Spawn;
+    using Exiled.Events.EventArgs;
+    using Grenades;
+    using Mirror;
+    using YamlDotNet.Serialization;
+
+    using PlayerEvent = Exiled.Events.Handlers.Player;
+    using ServerEvent = Exiled.Events.Handlers.Server;
+    using MapEvent = Exiled.Events.Handlers.Map;
+    using UnityEngine;
+
+    /// <inheritdoc/>
+    public class C4Charge : CustomGrenade
+    {
+        /// <inheritdoc/>
+        public static C4Charge Instance;
+
+        /// <inheritdoc/>
+        public static Dictionary<Grenade, Exiled.API.Features.Player> PlacedCharges = new Dictionary<Grenade, Exiled.API.Features.Player>();
+
+        /// <inheritdoc/>
+        public override uint Id { get; set; } = 15;
+
+        /// <inheritdoc/>
+        public override string Name { get; set; } = "C4-119";
+
+        /// <inheritdoc/>
+        public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties
+        {
+            Limit = 5,
+            DynamicSpawnPoints = new List<DynamicSpawnPoint>
+            {
+                new DynamicSpawnPoint
+                {
+                    Chance = 10,
+                    Location = SpawnLocation.InsideLczArmory,
+                },
+
+                new DynamicSpawnPoint
+                {
+                    Chance = 25,
+                    Location = SpawnLocation.InsideHczArmory,
+                },
+
+                new DynamicSpawnPoint
+                {
+                    Chance = 50,
+                    Location = SpawnLocation.InsideNukeArmory,
+                },
+
+                new DynamicSpawnPoint
+                {
+                    Chance = 50,
+                    Location = SpawnLocation.Inside049Armory,
+                },
+
+                new DynamicSpawnPoint
+                {
+                    Chance = 100,
+                    Location = SpawnLocation.InsideSurfaceNuke,
+                },
+            },
+        };
+
+        /// <inheritdoc/>
+        public override string Description { get; set; } = "Explosive charge that can be remotly detonated.";
+
+
+        /// <summary>
+        /// Gets or sets a value indicating whether C4 charge should stick to walls / ceiling.
+        /// </summary>
+        [Description("Should C4 charge stick to walls / ceiling.")]
+        public bool IsSticky { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating throwing force muliplier.
+        /// </summary>
+        [Description("Defines how strongly C4 will be thrown")]
+        public float ThrowMultiplier { get; set; } = 40f;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether C4 charge require a specific item to be detonated.
+        /// </summary>
+        [Description("Should C4 require a specific item to be detonated.")]
+        public bool RequireDetonator { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Detonator Item that will be used to detonate C4 Charges.
+        /// </summary>
+        [Description("The Detonator Item that will be used to detonate C4 Charges")]
+        public ItemType DetonatorItem { get; set; } = ItemType.Radio;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether C4 should C4 charges be detonated when the player who placed them dies.
+        /// </summary>
+        [Description("Should C4 charges be detonated when the player who placed them dies.")]
+        public bool AutoDetonate { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether maximum distance between C4 Charge and player to detonate.
+        /// </summary>
+        [Description("Maximum distance between C4 Charge and player to detonate.")]
+        public float MaxDistance { get; set; } = 100f;
+
+        /// <summary>
+        /// Gets or sets time after which the C4 charge will automatically detonate.
+        /// </summary>
+        [Description("Time after which the C4 charge will automatically detonate.")]
+        public override float FuseTime { get; set; } = 9999f;
+
+        /// <inheritdoc/>
+        [YamlIgnore]
+        public override bool ExplodeOnCollision { get; set; } = false;
+
+
+        /// <inheritdoc/>
+        protected override void SubscribeEvents()
+        {
+            Instance = this;
+
+            ServerEvent.WaitingForPlayers += OnWaitingForPlayers;
+
+            PlayerEvent.ThrowingGrenade += OnThrowing;
+            PlayerEvent.Destroying += OnDestroying;
+            PlayerEvent.Died += OnDied;
+
+            MapEvent.ExplodingGrenade += OnExploding;
+
+            base.SubscribeEvents();
+        }
+
+        /// <inheritdoc/>
+        protected override void UnsubscribeEvents()
+        {
+            Instance = null;
+
+            ServerEvent.WaitingForPlayers -= OnWaitingForPlayers;
+
+            PlayerEvent.ThrowingGrenade -= OnThrowing;
+            PlayerEvent.Destroying -= OnDestroying;
+            PlayerEvent.Died -= OnDied;
+
+            MapEvent.ExplodingGrenade += OnExploding;
+
+            base.UnsubscribeEvents();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnWaitingForPlayers()
+        {
+            PlacedCharges.Clear();
+
+            base.OnWaitingForPlayers();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnThrowing(ThrowingGrenadeEventArgs ev)
+        {
+            if (Check(ev.Player.CurrentItem))
+            {
+                ev.IsAllowed = false;
+
+                ev.Player.RemoveItem(ev.Player.CurrentItem);
+
+                var c4 = Spawn(ev.Player.CameraTransform.position, (ev.Player.Rotation * ThrowMultiplier) + (Vector3.up * 1.5f), FuseTime);
+
+                c4.NetworkthrowerGameObject = ev.Player.GameObject;
+                c4.NetworkthrowerTeam = ev.Player.Team;
+
+                if (!PlacedCharges.ContainsKey(c4))
+                    PlacedCharges.Add(c4, ev.Player);
+            }
+
+            base.OnThrowing(ev);
+        }
+
+        private void OnDestroying(DestroyingEventArgs ev)
+        {
+            foreach (var charge in PlacedCharges.ToList())
+            {
+                if (charge.Value == ev.Player)
+                {
+                    if (AutoDetonate)
+                        charge.Key.NetworkfuseTime = 0.1f;
+                    else
+                        NetworkServer.Destroy(charge.Key.gameObject);
+
+                    PlacedCharges.Remove(charge.Key);
+                }
+            }
+        }
+
+        private void OnDied(DiedEventArgs ev)
+        {
+            foreach (var charge in PlacedCharges.ToList())
+            {
+                if (charge.Value == ev.Target)
+                {
+                    if (AutoDetonate)
+                        charge.Key.NetworkfuseTime = 0.1f;
+                    else
+                        NetworkServer.Destroy(charge.Key.gameObject);
+
+                    PlacedCharges.Remove(charge.Key);
+                }
+            }
+        }
+
+        private void OnExploding(ExplodingGrenadeEventArgs ev)
+        {
+            if (PlacedCharges.ContainsKey(ev.Grenade.GetComponent<Grenade>()))
+            {
+                PlacedCharges.Remove(ev.Grenade.GetComponent<Grenade>());
+            }
+        }
+    }
+}
---
+++ CustomItems/Patches/StickyGrenade.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomItems.Patches
+{
+    using Grenades;
+    using HarmonyLib;
+    using UnityEngine;
+
+    /// <inheritdoc/>
+    [HarmonyPatch(typeof(Grenade), nameof(Grenade.OnCollisionEnter))]
+    public class StickyGrenade
+    {
+        /// <inheritdoc/>
+        public static void Prefix(Grenade __instance)
+        {
+            if (Items.C4Charge.Instance.IsSticky && Items.C4Charge.PlacedCharges.ContainsKey(__instance))
+            {
+                var rigidbody = __instance.gameObject.GetComponent<Rigidbody>();
+
+                rigidbody.isKinematic = false;
+                rigidbody.useGravity = false;
+                rigidbody.velocity = Vector3.zero;
+                rigidbody.angularVelocity = Vector3.zero;
+                rigidbody.freezeRotation = true;
+            }
+        }
+    }
+}
